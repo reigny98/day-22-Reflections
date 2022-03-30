0828a9f01bd1112eb8ef8bc9e20363c8ebc7c2e9
reigny98 <reigny2016@gmail.com>
2022-03-29 20:13:39 -1200

    added

---
+++ UC3CustomerExceptions/UC3CustomerExceptions/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReflectionsUseCasesDay22
+{
+    public class MoodAnalyser
+    {
+        public string message;
+        public MoodAnalyser(string message)
+        {
+            this.message = message;
+        }
+
+        public string AnalyserMood()
+        {
+            //try
+            //{
+            //    if (this.message.Contains("Sad"))
+            //    {
+            //        return "SAD";
+            //    }
+            //    else
+            //    {
+            //        return "HAPPY";
+            //    }
+            //}
+            //catch
+            //{
+            //    return "HAPPY";
+
+            //}
+            try
+            {
+                if (this.message.Equals(string.Empty))
+                {
+                    throw new MoodAnalyserCustomExeption(MoodAnalyserCustomExeption.ExceptionType.Empty_Message, "Mood Should Not be Empty");
+                }
+                if (this.message.Contains("Sad"))
+                {
+                    return "SAD";
+                }
+                else
+                {
+                    return "HAPPY";
+                }
+            }
+            catch (NullReferenceException)
+            {
+                throw new MoodAnalyserCustomExeption(MoodAnalyserCustomExeption.ExceptionType.Null_Message, "Mood Should Not be Null");
+            }
+        }
+    }
+}
