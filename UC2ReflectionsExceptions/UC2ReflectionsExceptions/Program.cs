8aea3ee1c52fa2ebbd0f8a3d60f816aec92dbb67
reigny98 <reigny2016@gmail.com>
2022-03-29 20:05:27 -1200

    added

---
+++ UC2ReflectionsExceptions/UC2ReflectionsExceptions/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReflectionsUseCasesDay22
+{
+    public class MoodAnalyser
+    {
+        public string message;
+        public MoodAnalyser(string message)
+        {
+            this.message = message;
+        }
+
+        public string AnalyserMood()
+        {
+            try
+            {
+                if (this.message.Contains("Sad"))
+                {
+                    return "SAD";
+                }
+                else
+                {
+                    return "HAPPY";
+                }
+
+            }
+            catch
+            {
+                return "HAPPY";
+
+            }
+        }
+    }
+}
