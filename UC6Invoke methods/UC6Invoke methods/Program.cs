d3b9aca98127a555a99c0e3ec7ea8a01100b6b47
reigny98 <reigny2016@gmail.com>
2022-03-29 20:20:48 -1200

    added

---
+++ UC6Invoke methods/UC6Invoke methods/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReflectionsUseCasesDay22
+{
+    public class MoodAnalyser
+    {
+        public MoodAnalyser() { }
+        public string message;
+        public MoodAnalyser(string message)
+        {
+            this.message = message;
+        }
+
+        public string AnalyserMood()
+        {
+            //try
+            //{
+            //    if (this.message.Contains("Sad"))
+            //    {
+            //        return "SAD";
+            //    }
+            //    else
+            //    {
+            //        return "HAPPY";
+            //    }
+            //}
+            //catch
+            //{
+            //    return "HAPPY";
+
+            //}
+            Console.WriteLine("Enter Your Mood");
+            message = Console.ReadLine();
+            try
+            {
+                if (this.message.Equals(string.Empty))
+                {
+                    throw new MoodAnalyserCustomExeption(MoodAnalyserCustomExeption.ExceptionType.No_Such_Field, "No Such Field Error");
+                }
+                if (this.message.Contains("Sad"))
+                {
+                    return "SAD";
+                }
+                else
+                {
+                    return "HAPPY";
+                }
+            }
+            catch (NullReferenceException)
+            {
+                throw new MoodAnalyserCustomExeption(MoodAnalyserCustomExeption.ExceptionType.Null_Message, "Mood Should Not be Null");
+            }
+        }
+    }
+}
